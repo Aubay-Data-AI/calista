a85f429dc4296ab26124aea78be85c072ed01550
root <root@DESKTOP-FTRKPHH.>
2025-02-27 14:44:00 +0100

    debut dev pour la verification des emails

---
+++ calista/label/spark/email/email.py
@@ -0,0 +1,22 @@
+import re
+from pyspark.sql import SparkSession
+from pyspark.sql.types import BooleanType
+from pyspark.sql.functions import udf, col
+
+
+spark = SparkSession.builder.appName("IBANValidation").getOrCreate()
+
+
+def verifier_email_regex(email:str) -> bool:
+
+    email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return bool(re.match(email_regex, email))
+
+is_valid_email = udf(verifier_email_regex, BooleanType())
+
+
+"""data = [("test@example.com",), ("mic@example.com",), ("peter@example.com",),("mauvais-email@",),("@com",)]
+df = spark.createDataFrame(data, ["email"])
+df=df.withColumn("emailValid",is_valid_email("email"))
+
+df.show()"""
