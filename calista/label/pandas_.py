e67622ea5dafa62fe0a463299c7243c71b7435c7
root <root@DESKTOP-FTRKPHH.>
2025-03-07 18:34:48 +0100

    dev des labels pour pandas

---
+++ calista/label/pandas_.py
@@ -0,0 +1,55 @@
+from email_validator import validate_email, EmailNotValidError
+import schwifty
+import validators
+import pandas as pd
+
+def is_valid_iban(iban: str) -> bool:
+    """
+     
+        is valid iban It will be used to check an Iban
+
+        Args:
+            the function takes a string iban as a argument
+
+        Returns:
+        bool: the function returns a boolean.
+
+    """
+    return bool(schwifty.IBAN(iban, allow_invalid=True).is_valid)
+
+
+def is_valid_email(email: str) -> bool:
+    """
+        is valid email It will be used to check an email
+
+        Args:
+            the function takes a string email as a argument
+
+        Returns:
+        bool: the function returns a boolean.
+
+    """
+    try:
+        validate_email(email, check_deliverability=False)
+        return True
+    except EmailNotValidError:
+        return False
+
+
+def is_valid_ip_adress(adress_ip: str) -> bool:
+    """
+        is valid ip_adress It will be used to check an ip_adress
+
+        Args:
+            the function takes a string email as a argument
+
+        Returns:
+        bool: the function returns a boolean.
+
+    """
+    return bool(validators.ipv4(adress_ip) or validators.ipv6(adress_ip))
+
+
+df = pd.read_csv('emails.csv')
+df['is_valid_email'] = df['email'].apply(is_valid_email)
+print(df.head(10))
