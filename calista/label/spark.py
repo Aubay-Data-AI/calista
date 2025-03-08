ba1757bd97b25b6a0dddf4e407d50ff730808884
root <root@DESKTOP-FTRKPHH.>
2025-03-08 10:57:36 +0100

    mis en forme du label spark

--- calista/label/spark.py
+++ calista/label/spark.py
@@ -7,21 +7,50 @@ import validators
 
 
 
-@udf(returnType=BooleanType(), useArrow=True)
+@udf(returnType= BooleanType(), useArrow=True)
 def is_valid_iban(iban: str) -> bool:
-    """ Vérifie si un IBAN est valide en utilisant schwifty """
+    """
+        is valid iban It will be used to check an ip_adress
+
+        Args:
+            the function takes a string iban as a argument
+
+        Returns:
+        bool: the function returns a boolean.
+
+    """
     return bool(schwifty.IBAN(iban, allow_invalid=True).is_valid)
 
 
-@udf(returnType=BooleanType(), useArrow=True)
+@udf(returnType= BooleanType(), useArrow=True)
 def is_valid_email(email: str) -> bool:
-    """Retourne True si l'email est valide, False sinon."""
+    """
+        is valid email It will be used to check an ip_adress
+
+        Args:
+            the function takes a string email as a argument
+
+        Returns:
+        bool: the function returns a boolean.
+
+    """
     try:
         validate_email(email, check_deliverability=False)
         return True
     except EmailNotValidError:
         return False
     
-@udf(returnType=BooleanType(),useArrow=True)
-def is_valid_ip_adress(adress_ip:str)->bool:
+
+@udf(returnType= BooleanType(), useArrow=True)
+def is_valid_ip_adress(adress_ip: str) -> bool:
+    """
+        is valid ip_adress It will be used to check an ip_adress
+
+        Args:
+            the function takes a string adress_ip as a argument
+
+        Returns:
+        bool: the function returns a boolean.
+
+    """
     return bool(validators.ipv4(adress_ip) or validators.ipv6(adress_ip))
