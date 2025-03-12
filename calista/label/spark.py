8f1512db720b7a999ed3d11146fc3299aaff28b8
root <root@DESKTOP-FTRKPHH.>
2025-03-12 14:24:19 +0100

    gestion des none

--- calista/label/spark.py
+++ calista/label/spark.py
@@ -6,7 +6,6 @@ import schwifty
 import validators
 
 
-
 @udf(returnType= BooleanType(), useArrow=True)
 def is_valid_iban(iban: str) -> bool:
     """
@@ -19,6 +18,8 @@ def is_valid_iban(iban: str) -> bool:
         bool: the function returns a boolean.
 
     """
+    if not iban:
+        return False
     return bool(schwifty.IBAN(iban, allow_invalid=True).is_valid)
 
 
@@ -34,6 +35,8 @@ def is_valid_email(email: str) -> bool:
         bool: the function returns a boolean.
 
     """
+    if not email: 
+        return False
     try:
         validate_email(email, check_deliverability=False)
         return True
@@ -42,15 +45,15 @@ def is_valid_email(email: str) -> bool:
     
 
 @udf(returnType= BooleanType(), useArrow=True)
-def is_valid_ip_adress(adress_ip: str) -> bool:
+def is_valid_ip_address(address_ip: str) -> bool:
     """
-        is valid ip_adress It will be used to check an ip_adress
+        is valid ip_address It will be used to check an ip_address
 
         Args:
-            the function takes a string adress_ip as a argument
+            the function takes a string address_ip as a argument
 
         Returns:
         bool: the function returns a boolean.
 
     """
-    return bool(validators.ipv4(adress_ip) or validators.ipv6(adress_ip))
+    return bool(validators.ipv4(address_ip) or validators.ipv6(address_ip))
